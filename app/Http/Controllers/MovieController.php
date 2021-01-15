33cf2f71e9abc7fad95f6398e124735939fffeec
jferland09 <55703115+jferland09@users.noreply.github.com>
2021-01-15 17:34:09 -0500

    Added checks to store and moved code from index to show

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -21,58 +21,7 @@ class MovieController extends Controller
      */
     public function index()
     {
-        $movie = Movie::find(27);
-        $return_data = [
-            "title" => $movie->title,
-            "description" => $movie->description,
-            "directors" => [],
-            "writers" => [],
-            "stars" => [],
-            "genres" => [],
-            "rating_value" => $movie->rating_value,
-            "rating_description" => $movie->rating_description,
-            "countries" => [],
-            "languages" => [],
-            "release_date" => $movie->release_date,
-            "runtime" => $movie->runtime
-        ];
-
-        foreach ($movie->directors as $director) {
-            $return_data["directors"] []= [
-                "first_name" => $director->first_name, 
-                "last_name" => $director->last_name
-            ];
-        }
-
-        foreach ($movie->writers as $writer) {
-            $return_data["writers"] []= [
-                "first_name" => $writer->first_name, 
-                "last_name" => $writer->last_name, 
-                "specialization" => $writer->pivot->specialization
-            ];
-        }
-
-        foreach ($movie->stars as $star) {
-            $return_data["stars"] []= [
-                "first_name" => $star->first_name,
-                "last_name" => $star->last_name,
-                "role" => $star->pivot->role
-            ];
-        }
-
-        foreach ($movie->genres as $genre) {
-            $return_data["genres"] []= $genre->name;
-        }
-
-        foreach ($movie->countries as $country) {
-            $return_data["countries"] []= $country->name;
-        }
-
-        foreach ($movie->languages as $language) {
-            $return_data["languages"] []= $language->name;
-        }
-
-        return response($return_data);
+        
     }
 
     /**
@@ -83,133 +32,201 @@ class MovieController extends Controller
      */
     public function store(Request $request)
     {
-        $new_movie = new Movie;
-        $new_movie->title = $request->title;
-        $new_movie->description = $request->description;
-        $new_movie->rating_value = $request->rating_value;
-        $new_movie->rating_description = $request->rating_description;
-        $new_movie->release_date = $request->release_date;
-        $new_movie->runtime = $request->runtime;
-        $new_movie->save();
+        if (Movie::where("title", $request->title)->first()) {
+            $return_msg = response("Cannot add an already existing movie", 500);
+        } else {
+            $new_movie = new Movie;
+            $new_movie->title = $request->title;
+            $new_movie->description = $request->description;
+            $new_movie->rating_value = $request->rating_value;
+            $new_movie->rating_description = $request->rating_description;
+            $new_movie->release_date = $request->release_date;
+            $new_movie->runtime = $request->runtime;
 
-        foreach ($request->directors as $director) {
-            $existing_director = Director::where([
-                "first_name" => $director["first_name"],
-                "last_name" => $director["last_name"]
-            ])->first();
+            if ($new_movie->save()) {
+                foreach ($request->directors as $director) {
+                    $existing_director = Director::where([
+                        "first_name" => $director["first_name"],
+                        "last_name" => $director["last_name"]
+                    ])->first();
 
-            if ($existing_director) {
-                $new_movie->directors()->attach($existing_director->id);
-            } else {
-                $new_movie->directors()->create([
-                    "first_name" => $director["first_name"],
-                    "last_name" => $director["last_name"]
-                ]);
-            }
-        }
-
-        foreach ($request->writers as $writer) {
-            $existing_writer = Writer::where([
-                "first_name" => $writer["first_name"],
-                "last_name" => $writer["last_name"]
-            ])->first();
-            
-            if ($existing_writer) {
-                if (isset($writer["specialization"])) {
-                    $new_movie->writers()->attach($existing_writer->id, [
-                        "specialization" => $writer["specialization"]
-                    ]);
-                } else {
-                    $new_movie->writers()->attach($existing_writer->id);
+                    if ($existing_director) {
+                        $new_movie->directors()->attach($existing_director->id);
+                    } else {
+                        $new_movie->directors()->create([
+                            "first_name" => $director["first_name"],
+                            "last_name" => $director["last_name"]
+                        ]);
+                    }
                 }
-            } else {
-                $new_writer = $new_movie->writers()->create([
-                    "first_name" => $writer["first_name"],
-                    "last_name" => $writer["last_name"]
-                ]);
 
-                if (isset($writer["specialization"])) {
-                    $new_movie->writers()->updateExistingPivot($new_writer->id, [
-                        "specialization" => $writer["specialization"]
-                    ]);
+                foreach ($request->writers as $writer) {
+                    $existing_writer = Writer::where([
+                        "first_name" => $writer["first_name"],
+                        "last_name" => $writer["last_name"]
+                    ])->first();
+                    
+                    if ($existing_writer) {
+                        if (isset($writer["specialization"])) {
+                            $new_movie->writers()->attach($existing_writer->id, [
+                                "specialization" => $writer["specialization"]
+                            ]);
+                        } else {
+                            $new_movie->writers()->attach($existing_writer->id);
+                        }
+                    } else {
+                        $new_writer = $new_movie->writers()->create([
+                            "first_name" => $writer["first_name"],
+                            "last_name" => $writer["last_name"]
+                        ]);
+
+                        if (isset($writer["specialization"])) {
+                            $new_movie->writers()->updateExistingPivot($new_writer->id, [
+                                "specialization" => $writer["specialization"]
+                            ]);
+                        }
+                    }
                 }
-            }
-        }
 
-        foreach ($request->stars as $star) {
-            $existing_star = Star::where([
-                "first_name" => $star["first_name"],
-                "last_name" => $star["last_name"]
-            ])->first();
+                foreach ($request->stars as $star) {
+                    $existing_star = Star::where([
+                        "first_name" => $star["first_name"],
+                        "last_name" => $star["last_name"]
+                    ])->first();
 
-            if ($existing_star) {
-                if ($star["role"]) {
-                    $new_movie->stars()->attach($existing_star->id, [
-                        "role" => $star["role"]
-                    ]);
-                } else {
-                    $new_movie->stars()->attach($existing_star->id);
+                    if ($existing_star) {
+                        if ($star["role"]) {
+                            $new_movie->stars()->attach($existing_star->id, [
+                                "role" => $star["role"]
+                            ]);
+                        } else {
+                            $new_movie->stars()->attach($existing_star->id);
+                        }
+                    } else {
+                        $new_star = $new_movie->stars()->create([
+                            "first_name" => $star["first_name"],
+                            "last_name" => $star["last_name"]
+                        ]);
+
+                        if ($star["role"]) {
+                            $new_movie->stars()->updateExistingPivot($new_star->id, [
+                                "role" => $star["role"]
+                            ]);
+                        }
+                    }
                 }
-            } else {
-                $new_star = $new_movie->stars()->create([
-                    "first_name" => $star["first_name"],
-                    "last_name" => $star["last_name"]
-                ]);
 
-                if ($star["role"]) {
-                    $new_movie->stars()->updateExistingPivot($new_star->id, [
-                        "role" => $star["role"]
-                    ]);
+                foreach ($request->genres as $genre) {
+                    $existing_genre = Genre::where("name", $genre)->first();
+
+                    if ($existing_genre) {
+                        $new_movie->genres()->attach($existing_genre->id);
+                    } else {
+                        $new_movie->genres()->create([
+                            "name" => $genre
+                        ]);
+                    }
                 }
-            }
-        }
 
-        foreach ($request->genres as $genre) {
-            $existing_genre = Genre::where("name", $genre)->first();
+                foreach ($request->countries as $country) {
+                    $existing_country = Country::where("name", $country)->first();
 
-            if ($existing_genre) {
-                $new_movie->genres()->attach($existing_genre->id);
+                    if ($existing_country) {
+                        $new_movie->countries()->attach($existing_country->id);
+                    } else {
+                        $new_movie->countries()->create([
+                            "name" => $country
+                        ]);
+                    }
+                }
+
+                foreach ($request->languages as $language) {
+                    $existing_language = Language::where("name", $language)->first();
+
+                    if ($existing_language) {
+                        $new_movie->languages()->attach($existing_language->id);
+                    } else {
+                        $new_movie->languages()->create([
+                            "name" => $country
+                        ]);
+                    }
+                }
+
+                $return_msg = response("Movie information saved successfully", 200);
             } else {
-                $new_movie->genres()->create([
-                    "name" => $genre
-                ]);
+                $return_msg = response("There was an error saving movie to the system", 500);
             }
         }
 
-        foreach ($request->countries as $country) {
-            $existing_country = Country::where("name", $country)->first();
-
-            if ($existing_country) {
-                $new_movie->countries()->attach($existing_country->id);
-            } else {
-                $new_movie->countries()->create([
-                    "name" => $country
-                ]);
-            }
-        }
-
-        foreach ($request->languages as $language) {
-            $existing_language = Language::where("name", $language)->first();
-
-            if ($existing_language) {
-                $new_movie->languages()->attach($existing_language->id);
-            } else {
-                $new_movie->languages()->create([
-                    "name" => $country
-                ]);
-            }
-        }
+        return $return_msg;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int  $movie_id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($movie_id)
     {
-        //
+        $movie = Movie::find($movie_id);
+        if ($movie) {
+            $return_data = [
+                "title" => $movie->title,
+                "description" => $movie->description,
+                "directors" => [],
+                "writers" => [],
+                "stars" => [],
+                "genres" => [],
+                "rating_value" => $movie->rating_value,
+                "rating_description" => $movie->rating_description,
+                "countries" => [],
+                "languages" => [],
+                "release_date" => $movie->release_date,
+                "runtime" => $movie->runtime
+            ];
+
+            foreach ($movie->directors as $director) {
+                $return_data["directors"] []= [
+                    "first_name" => $director->first_name, 
+                    "last_name" => $director->last_name
+                ];
+            }
+
+            foreach ($movie->writers as $writer) {
+                $return_data["writers"] []= [
+                    "first_name" => $writer->first_name, 
+                    "last_name" => $writer->last_name, 
+                    "specialization" => $writer->pivot->specialization
+                ];
+            }
+
+            foreach ($movie->stars as $star) {
+                $return_data["stars"] []= [
+                    "first_name" => $star->first_name,
+                    "last_name" => $star->last_name,
+                    "role" => $star->pivot->role
+                ];
+            }
+
+            foreach ($movie->genres as $genre) {
+                $return_data["genres"] []= $genre->name;
+            }
+
+            foreach ($movie->countries as $country) {
+                $return_data["countries"] []= $country->name;
+            }
+
+            foreach ($movie->languages as $language) {
+                $return_data["languages"] []= $language->name;
+            }
+
+            $return_msg = response($return_data, 200);
+        } else {
+            $return_msg = response("No movie found for movie ID provided", 404);
+        }
+
+        return $return_msg;
     }
 
     /**
