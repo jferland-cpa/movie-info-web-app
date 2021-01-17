c82dd605ac2e3b0990e020536bf92c25eb861aa0
jferland09 <55703115+jferland09@users.noreply.github.com>
2021-01-17 11:16:05 -0500

    Added Vue front-end and movie list vue component

---
+++ resources/js/components/MovieList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Movie List</div>
+                    <div class="card-body">
+                        <p v-for="(movie, index) in movies" :key="index">{{ movie.title }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                movies: []
+            }
+        }, mounted() {
+            axios.get("/api/movies").then(response => {
+                this.movies = response.data;
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+    }
+</script>
